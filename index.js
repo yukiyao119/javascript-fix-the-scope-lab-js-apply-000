4bb3223f62ca40be9a4901d275d144b2308278aa
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-04-29 01:29:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,12 +28,13 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-let innerFunc = function() {
-    return "FUNKY!"
-  }
+// let innerFunc = function() {
+//     return "FUNKY!"
+//   }
   
-innerFunc();
+// innerFunc();
 
-//var theFunk = funkyFunction();
+var theFunk = funkyFunction();
+theFunk();
 
 
