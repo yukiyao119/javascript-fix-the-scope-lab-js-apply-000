625c5504d61379e2bdcb5edec5187a60c321a430
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-04-29 02:48:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = funkyFunction();
+var theFunk = funkyFunction('');
 //theFunk now is the returned anonymous function name inside funkyFunction
 //the stringified version of `anonymous()`
 //it enabled funkyFunction's argu, but not anonymous func;
 
-theFunk();
+theFunk('');
 
 
 
