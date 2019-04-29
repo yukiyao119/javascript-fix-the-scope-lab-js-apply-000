63a510ea29d850775c6a8d5b8d631859d3db1a04
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-04-29 01:47:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,10 +28,9 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-var theFunk = funkyFunction = function(){
-  innerFunc();
-};
+var theFunk = funkyFunction;
 //theFunk now is the returned anonymous function name inside funkyFunction
+
 let innerFunc = function(){
   return "FUNKY!"
 }
